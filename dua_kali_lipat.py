503509785d1bd9da766cdd7709c0e086cfc53f1f
Rion <rioneffendy0001repo@gmail.com>
2024-07-15 21:43:42 +0700

    Create dua_kali_lipat.py

---
+++ dua_kali_lipat.py
@@ -0,0 +1,24 @@
+"""
+DUA KALI LIPAT
+
+Deskripsi :
+Buatlah program yang menampilkan bilangan yang tepat dua kali lipat dari bilangan yang berasal dari inputan pengguna.
+
+Format Input :
+Satu baris berisi sebuah bilangan bulat X (0 <= X <= 100)
+
+Format Output :
+Satu baris berisi sebuah bilangan bulat yang tepat dua kali lebih besar dari X.
+
+Contoh :
+
+Input    Result
+7        14
+45       90
+21       42
+70       140
+"""
+
+bilangan_x = int(input("-> "))
+hasil = bilangan_x * 2
+print(hasil)
